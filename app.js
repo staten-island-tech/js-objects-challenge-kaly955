ddd61e417235b48f44d516e424d7695cf2b4adec
kaly955 <57500215+kaly955@users.noreply.github.com>
2020-04-04 05:24:20 -0400

    a

---
+++ app.js
@@ -0,0 +1,34 @@
+//Challenge Questions
+//1) Filter all presidents, leaving only the Democratic ones
+ 
+//2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+ 
+//3) return only the last three presidents
+ 
+//4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+
+
+const presidents = [
+    { name: "Donald Trump", terms: 1, party: "R" },
+    { name: "Barack Obama", terms: 2, party: "D" },
+    { name: "George W. Bush", terms: 2, party: "R" },
+    { name: "Bill Clinton", terms: 2, party: "D" },
+    { name: "George H.W. Bush", terms: 1, party: "R" },
+    { name: "Ronald Reagan", terms: 2, party: "R" },
+    { name: "Jimmy Carter", terms: 1, party: "D" },
+    { name: "Gerlad Ford", terms: 1, party: "R" },
+    { name: "Richard Nixon", terms: 2, party: "R" },
+    { name: "John F. Kennedy", terms: 1, party: "D" },
+    { name: "Lyndon Johnson", terms: 2, party: "D" },
+    { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  ];
+
+
+// //BONUS
+// let Lyndon = presidents[10];
+
+//  if (Lyndon.terms= 2){
+//      console.log("LBJ served two terms");
+//  } else {
+//      console.log("LBJ was one and done");
+//  }
